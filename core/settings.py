23682e91889f942e97fee3c3a47b6e57d18cff29
ahmedshahriar <ahmed.shahriar.sakib@gmail.com>
2025-10-06 22:38:56 -0600

    chore: setting env init

---
+++ core/settings.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # seed for reproducibility
+    SEED: int = 42
+    PYTHONHASHSEED: str = str(SEED)
+
+    # kaggle config
+    KAGGLE_USERNAME: str | None = None
+    KAGGLE_KEY: str | None = None
+
+    # MongoDB database
+    DATABASE_HOST: str | None = None
+    DATABASE_NAME: str | None = None
+
+    # huggingface token
+    HUGGINGFACE_ACCESS_TOKEN: str | None = None
+
+
+settings = Settings()
